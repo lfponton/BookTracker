edd40b1409c17c79f1a54c3a2bb7620faa5b1c2e
Luis <lfponton@gmail.com>
2022-05-01 15:40:08 +0200

    sign in implemented

---
+++ app/src/main/java/com/example/booktracker/SignInActivity.java
@@ -0,0 +1,63 @@
+package com.example.booktracker;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.booktracker.viewmodels.SignInViewModel;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity {
+    private SignInViewModel viewModel;
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK)
+                    goToMainActivity();
+                else
+                    Toast.makeText(this, "SIGN IN CANCELLED", Toast.LENGTH_SHORT).show();
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(SignInViewModel.class);
+        checkIfSignedIn();
+        setContentView(R.layout.signin_activity);
+    }
+
+    private void checkIfSignedIn() {
+        viewModel.getCurrentUser().observe(this, user -> {
+            if (user != null)
+                goToMainActivity();
+        });
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    public void signIn(View v) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                //.setLogo(R.drawable.logo)
+                .build();
+
+        activityResultLauncher.launch(signInIntent);
+    }
+}
