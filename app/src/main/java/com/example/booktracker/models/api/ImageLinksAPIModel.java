cc7929bdc6a55d61931fc21f2aa7374ed6973832
Luis <lfponton@gmail.com>
2022-05-04 20:32:44 +0200

    added card with title, author and cover

--- app/src/main/java/com/example/booktracker/models/api/ImageLinksAPIModel.java
+++ app/src/main/java/com/example/booktracker/models/api/ImageLinksAPIModel.java
@@ -4,9 +4,29 @@ import java.io.Serializable;
 
 public class ImageLinksAPIModel implements Serializable {
     private String thumbnail;
+    private String medium;
+    private String large;
 
-    public ImageLinksAPIModel(String thumbnail) {
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getLarge() {
+        return large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public ImageLinksAPIModel(String thumbnail, String medium, String large) {
         this.thumbnail = thumbnail;
+        this.medium = medium;
+        this.large = large;
     }
 
     public String getThumbnail() {
--- app/src/main/java/com/example/booktracker/views/SearchedBookDetailsFragment.java
+++ app/src/main/java/com/example/booktracker/views/SearchedBookDetailsFragment.java
@@ -10,8 +10,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.booktracker.R;
 import com.example.booktracker.adapters.ItemAPIModelAdapter;
 import com.example.booktracker.models.api.ItemAPIModel;
@@ -19,17 +21,30 @@ import com.example.booktracker.viewmodels.BooksViewModel;
 
 public class SearchedBookDetailsFragment extends Fragment {
 
-    TextView text;
+    TextView bookTitle;
+    TextView bookAuthor;
+    ImageView cover;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_searched_book_details, container, false);
-        text = view.findViewById(R.id.bookDetailsName);
+        bookTitle = view.findViewById(R.id.bookDetailsName);
+        bookAuthor = view.findViewById(R.id.bookDetailsAuthor);
+        cover = view.findViewById(R.id.bookDetailsCover);
         BooksViewModel viewModel = new ViewModelProvider(this).get(BooksViewModel.class);
         ItemAPIModel book = viewModel.getSelectedBook();
-        text.setText(book.getVolumeInfo().getTitle());
+        bookTitle.setText(book.getVolumeInfo().getTitle());
+        bookAuthor.setText(book.getVolumeInfo().getAuthors());
+
+        String url = book.getVolumeInfo().getImageLinks().getThumbnail();
+        Glide.with(this)
+                .asBitmap()
+                .load(url)
+                .placeholder(R.drawable.ic_baseline_book_24)
+                .centerCrop()
+                .into(cover);
         return view;
     }
 
